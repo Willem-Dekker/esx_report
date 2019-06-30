1d9e6e961ac08ecb2195b81d137a9b022b47632b
Willem Spoelstra <16556575+WillemSpoelstra@users.noreply.github.com>
2019-06-30 15:58:04 +0200

    Add files via upload

--- locales/en.lua
+++ locales/en.lua
@@ -2,7 +2,7 @@ Locales['en'] = {
     ['send_to_admins']              = 'Report sent to the admins online!',
     ['help_text_id']                = 'ID of the player that you want to reply to',
     ['help_text_messege_reply']     = 'Message you want to send to the player',
-    ['help_text_message_report']    = 'Message you want to send to the admin',
+    ['help_text_messege_report']    = 'Message you want to send to the admin',
     ['description_report']          = 'Send a report to a admin',
-    ['descriprion_reply']           = 'Reply to a report that was given',
+    ['discription_reply']           = 'Reply to a report that was given',
 }
--- locales/nl.lua
+++ locales/nl.lua
@@ -2,7 +2,7 @@ Locales['en'] = {
     ['send_to_admins']              = 'Report is naar de admins online gestuurd!',
     ['help_text_id']                = 'ID van de speler waar je op wil andwoorden',
     ['help_text_messege_reply']     = 'Bericht is verzonden naar de speler',
-    ['help_text_message_report']    = 'Bericht is verzonden naar de admin',
+    ['help_text_messege_report']    = 'Bericht is verzonden naar de admin',
     ['description_report']          = 'Verstuur een report naar een admin',
-    ['descriprion_reply']           = 'Geef andwoordt op een report',
+    ['discription_reply']           = 'Geef andwoordt op een report',
 }
